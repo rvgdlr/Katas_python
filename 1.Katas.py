8d6e2d34b3460a7f72f9b07d7a7dc9f5bc25f7e8
rvgdlr <rvgdlr@gmail.com>
2025-11-11 18:37:49 +0000

    Comentar función

--- 1.Katas.py
+++ 1.Katas.py
@@ -1,7 +1,9 @@
+ # 1. Escribe una función que reciba una cadena de texto como parámetro y devuelva un diccionario con las frecuencias de cada letra en la cadena. Los espacios no deben ser considerados.
+
 def cadena_frecuencias(texto):
-    frecuencias = {}
+    frecuencias = {} 
     for letra in texto:
-        if letra !="":
+        if letra !="": # Para no considerar espacios
             if letra in frecuencias:
                 frecuencias[letra] += 1
             else:
